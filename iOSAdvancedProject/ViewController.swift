e83a6c4f876e47ae8a54c90fb75f7c186d96be85
Ismail Gok <ismailgok33@gmail.com>
2022-05-21 02:00:30 -0400

    Initial Commit

---
+++ iOSAdvancedProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSAdvancedProject
+//
+//  Created by Ismail Gok on 2022-05-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
